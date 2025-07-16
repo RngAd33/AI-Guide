818d4fb78ad9ec5a51fc1ab97b7957810b4c867c
RngAd33 <yxbsyx@outlook.com>
2025-07-17 00:28:00 +0800

    RAG知识库开发 + 通用化

---
+++ src/test/java/com/rngad33/aiguide/rag/MyMultiQueryExpanderTest.java
@@ -0,0 +1,23 @@
+package com.rngad33.aiguide.rag;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.rag.Query;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class MyMultiQueryExpanderTest {
+
+    @Resource
+    private MyMultiQueryExpander myMultiQueryExpander;
+
+    @Test
+    void doExpand() {
+        List<Query> queries = myMultiQueryExpander.expand("");
+        Assertions.assertEquals(3, queries.size());
+    }
+
+}
