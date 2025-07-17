6e35916afbc4d9902135a72e09b47787b42a4b24
RngAd33 <yxbsyx@outlook.com>
2025-07-17 21:20:41 +0800

    RAG知识库开发 + 通用化

---
+++ src/main/java/com/rngad33/aiguide/constant/SystemPromptsConstant.java
@@ -0,0 +1,12 @@
+package com.rngad33.aiguide.constant;
+
+/**
+ * 系统提示词常量
+ */
+public interface SystemPromptsConstant {
+
+    String PSYCHOLOGY = "你是一位二次元心理咨询师，" +
+            "当客户向你提问时，你需要对其进行答疑解惑，提供心理疏导；" +
+            "此外，在回答客户问题时，尽量带上一些颜文字以展现亲和力。";
+
+}
