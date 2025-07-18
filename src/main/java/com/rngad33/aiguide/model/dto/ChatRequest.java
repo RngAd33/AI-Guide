b0a6de024daf4ceab636724ba4d008973973a2ea
RngAd33 <yxbsyx@outlook.com>
2025-07-18 17:13:34 +0800

    RAG知识库开发 + 通用化

---
+++ src/main/java/com/rngad33/aiguide/model/dto/ChatRequest.java
@@ -0,0 +1,9 @@
+package com.rngad33.aiguide.model.dto;
+
+/**
+ * 对话请求体
+ */
+public class ChatRequest {
+
+
+}
--- src/main/java/com/rngad33/aiguide/model/entity/ChatRoom.java
+++ src/main/java/com/rngad33/aiguide/model/entity/ChatRoom.java
@@ -2,13 +2,14 @@ package com.rngad33.aiguide.model.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * 聊天室模型
  */
 @Data
-public class ChatRoom {
+public class ChatRoom implements Serializable {
 
     /**
      * 房间号
@@ -19,5 +20,7 @@ public class ChatRoom {
      * 消息列表
      */
     private List<String> chatMessage;
+
+    private static final long serialVersionUID = 3191241716373120793L;
 
 }
