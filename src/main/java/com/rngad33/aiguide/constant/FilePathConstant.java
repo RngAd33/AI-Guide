7c9dddefea79c4766dd0d19b17cfad1bdfb12faf
RngAd33 <yxbsyx@outlook.com>
2025-07-20 19:55:33 +0800

    工具开发与调用 + 通用化

---
+++ src/main/java/com/rngad33/aiguide/constant/FilePathConstant.java
@@ -0,0 +1,12 @@
+package com.rngad33.aiguide.constant;
+
+/**
+ * 文件路径常量
+ */
+public interface FilePathConstant {
+
+    String FILE_MEMORY_PATH = System.getProperty("user.dir") + "/tmp/chatHistory1";
+
+    String FILE_SAVE_PATH = System.getProperty("user.dir") + "/tmp";
+
+}
---
+++ src/main/java/com/rngad33/aiguide/tools/FileOperationTool.java
@@ -0,0 +1,30 @@
+package com.rngad33.aiguide.tools;
+
+/**
+ * 文件操作工具（提供文件读写功能）
+ */
+public class FileOperationTool {
+
+    /**
+     * 文件读取
+     *
+     * @param fileName
+     * @return
+     */
+    public String doRead(String fileName) {
+        return null;
+    }
+
+    /**
+     * 文件写入
+     *
+     * @param fileName
+     * @param content
+     * @return
+     */
+    public String doWrite(String fileName, String content) {
+        return null;
+    }
+
+
+}
