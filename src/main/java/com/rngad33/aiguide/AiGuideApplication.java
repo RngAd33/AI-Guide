e6a5ed8b374b5d565e6e5bf867432ac6f2e87476
RngAd33 <yxbsyx@outlook.com>
2025-07-20 14:01:51 +0800

    工具开发与调用 + 通用化

--- src/main/java/com/rngad33/aiguide/AiGuideApplication.java
+++ src/main/java/com/rngad33/aiguide/AiGuideApplication.java
@@ -1,5 +1,6 @@
 package com.rngad33.aiguide;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 项目启动入口
  */
 @SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
+@MapperScan("com.rngad33.aiguide.mapper")
 public class AiGuideApplication {
 
     public static void main(String[] args) {
