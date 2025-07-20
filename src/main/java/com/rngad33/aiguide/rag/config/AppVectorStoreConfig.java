a727c1260ef2e751afe234e9b64adbe7844f048a
RngAd33 <yxbsyx@outlook.com>
2025-07-20 13:45:39 +0800

    工具开发与调用 + 通用化

--- src/main/java/com/rngad33/aiguide/rag/config/AppVectorStoreConfig.java
+++ src/main/java/com/rngad33/aiguide/rag/config/AppVectorStoreConfig.java
@@ -1,21 +1,26 @@
 package com.rngad33.aiguide.rag.config;
 
+import com.rngad33.aiguide.exception.MyException;
+import com.rngad33.aiguide.model.enums.misc.ErrorCodeEnum;
 import com.rngad33.aiguide.rag.custom.MyKeywordEnricher;
 import com.rngad33.aiguide.rag.custom.MyTokenTextSplitter;
 import com.rngad33.aiguide.rag.documentloader.PsychologyAppDocumentLoader;
 import com.rngad33.aiguide.utils.AiModelUtils.MyEmbeddingModel;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 
 /**
  * 应用向量数据库配置
  */
+@Slf4j
 @Configuration
 public class AppVectorStoreConfig {
 
@@ -36,16 +41,21 @@ public class AppVectorStoreConfig {
      */
     @Bean("psychologyAppVectorStore")
     VectorStore psychologyAppVectorStore(MyEmbeddingModel embeddingModel) {
-        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
-        // 加载文档
-        List<Document> documents = psychologyAppDocumentLoader.loadMarkdowns();
-        // 切割文档
-        List<Document> splitDocuments = myTokenTextSplitter.splitDocuments(documents);
-        // simpleVectorStore.add(splitDocuments);
-        // 元信息增强
-        List<Document> enrichedDocuments = myKeywordEnricher.enrichDocuments(splitDocuments);
-        simpleVectorStore.add(enrichedDocuments);
-        return simpleVectorStore;
+        try {
+            SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+            // 加载文档
+            List<Document> documents = psychologyAppDocumentLoader.loadMarkdowns();
+            // 切割文档
+            List<Document> splitDocuments = myTokenTextSplitter.splitDocuments(documents);
+            // simpleVectorStore.add(splitDocuments);
+            // 元信息增强
+            List<Document> enrichedDocuments = myKeywordEnricher.enrichDocuments(splitDocuments);
+            simpleVectorStore.add(enrichedDocuments);
+            return simpleVectorStore;
+        } catch (RestClientException e) {
+            log.error("Ollama 服务连接失败: {}", e.getMessage());
+            throw new MyException(ErrorCodeEnum.USER_LOSE_ACTION);
+        }
     }
 
 }
