467504c3754996cd2d241ed341eda173b76ab6de
RngAd33 <yxbsyx@outlook.com>
2025-07-28 12:24:48 +0800

    MCP开发

--- src/test/java/com/rngad33/aiguide/app/PsychologyAppTest.java
+++ src/test/java/com/rngad33/aiguide/app/PsychologyAppTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 /**
  * 心理咨询应用测试类
@@ -53,7 +54,7 @@ class PsychologyAppTest {
     void doChatWithStream() {
         String chatId = UUID.randomUUID().toString();
         String message = "最近学业繁忙，考试将即，如何缓解焦虑？";
-        String answer = psychologyApp.doChatWithRag(message, chatId);
+        Flux<String> answer = psychologyApp.doChatWithStream(message, chatId);
         Assertions.assertNotNull(answer);
     }
 
