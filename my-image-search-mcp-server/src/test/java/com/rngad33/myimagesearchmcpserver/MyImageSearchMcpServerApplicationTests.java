579e4353039132c27b9346cb6584f30f716e6a82
RngAd33 <yxbsyx@outlook.com>
2025-07-28 10:39:58 +0800

    MCP服务开发

--- my-image-search-mcp-server/src/test/java/com/rngad33/myimagesearchmcpserver/MyImageSearchMcpServerApplicationTests.java
+++ my-image-search-mcp-server/src/test/java/com/rngad33/myimagesearchmcpserver/MyImageSearchMcpServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyImageSearchMcpApplicationTests {
+class MyImageSearchMcpServerApplicationTests {
 
     @Test
     void contextLoads() {
