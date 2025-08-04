b877187c27563c4db5b298e9531813a099a9b96d
RngAd33 <yxbsyx@outlook.com>
2025-08-04 15:43:06 +0800

    应用扩展 + 优化细节

--- src/main/java/com/rngad33/aiguide/controller/ChatController.java
+++ src/main/java/com/rngad33/aiguide/controller/ChatController.java
@@ -1,9 +1,12 @@
 package com.rngad33.aiguide.controller;
 
 import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.core.util.StrUtil;
+import com.rngad33.aiguide.app.LoveApp;
 import com.rngad33.aiguide.app.PsychologyApp;
 import com.rngad33.aiguide.common.BaseResponse;
+import com.rngad33.aiguide.model.dto.chat.ChatStartRequest;
 import com.rngad33.aiguide.model.enums.misc.ErrorCodeEnum;
 import com.rngad33.aiguide.utils.ResultUtils;
 import com.rngad33.aiguide.utils.ThrowUtils;
@@ -21,19 +24,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     @Resource
+    private LoveApp loveApp;
+
+    @Resource
     private PsychologyApp psychologyApp;
+
+    /**
+     * 恋爱大师
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/love")
+    public BaseResponse<String> loveChat(@RequestBody ChatStartRequest request) {
+        ThrowUtils.throwIf(ObjUtil.isNull(request), ErrorCodeEnum.PARAMS_ERROR, "无效的对话请求！");
+        String chatId = request.getChatId();
+        String message = request.getMessage();
+        String result = loveApp.doChat(message, chatId);
+        return ResultUtils.success(result);
+    }
 
     /**
      * 小姐姐心理疏导
      *
-     * @param prompt
+     * @param request
      * @return
      */
     @PostMapping("/psy")
-    public BaseResponse<String> psyChat(@RequestBody String prompt) {
-        ThrowUtils.throwIf(StrUtil.isBlank(prompt), ErrorCodeEnum.PARAMS_ERROR, "不可传入空提示词！");
+    public BaseResponse<String> psyChat(@RequestBody ChatStartRequest request) {
+        ThrowUtils.throwIf(ObjUtil.isNull(request), ErrorCodeEnum.PARAMS_ERROR, "无效的对话请求！");
         String chatId = UUID.randomUUID().toString();
-        String result = psychologyApp.doChat(prompt, chatId);
+        String message = request.getMessage();
+        String result = psychologyApp.doChat(message, chatId);
         return ResultUtils.success(result);
     }
 
