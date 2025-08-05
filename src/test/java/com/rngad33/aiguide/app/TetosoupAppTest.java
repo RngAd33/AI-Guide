783dab531ca45fd7c8a3405c0212e28f68bc9fb0
RngAd33 <yxbsyx@outlook.com>
2025-08-06 00:37:59 +0800

    应用拓展 + 优化细节

---
+++ src/test/java/com/rngad33/aiguide/app/TetosoupAppTest.java
@@ -0,0 +1,53 @@
+package com.rngad33.aiguide.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 海龟汤测试类
+ */
+@SpringBootTest
+class TetosoupAppTest {
+
+    @Resource
+    private TetosoupApp tetosoupApp;
+
+    @Test
+    void doChat() {
+        // start
+        String chatId = UUID.randomUUID().toString();
+        String message = "开始";
+        String answer = tetosoupApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // stop
+        message = "我不想玩了";
+        answer = tetosoupApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatByStream() {
+    }
+
+    @Test
+    void doChatWithReport() {
+    }
+
+    @Test
+    void doChatWithRag() {
+    }
+
+    @Test
+    void doChatWithTools() {
+    }
+
+    @Test
+    void doChatWithMcp() {
+    }
+
+}
