d23eff06d568cc4124de18e6b1815df1ff07aa26
RngAd33 <yxbsyx@outlook.com>
2026-02-13 10:47:17 +0800

    修复Agent代理功能； 新增用户管理、对话历史模块； 使用MariaDB代替PostgreSQL实现向量存储；

--- src/main/java/com/rngad33/aiguide/mapper/AppVectorMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rngad33.aiguide.mapper;
-
-import com.mybatisflex.core.BaseMapper;
-import com.rngad33.aiguide.model.entity.AppVector;
-
-/**
- * 应用向量 Mapper
- */
-public interface AppVectorMapper extends BaseMapper<AppVector> {}
